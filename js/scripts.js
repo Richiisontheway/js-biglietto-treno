3cedfddf7e1b28a25c645515e5f4899f8e3bfb55
Richiisontheway <mineiriccardo@gmail.com>
2023-12-01 16:34:43 +0100

    esercizio finto

--- js/scripts.js
+++ js/scripts.js
@@ -10,35 +10,36 @@ if (isNaN(KilometriDaPercorrere)) {
     alert('Scrivi un Numero Stronzo')
 }
 
-// Moneta
+//Moneta
 
 const Moneta = ('€')
 
-// Prezzo
+//Prezzo
 
-const PrezzoNormaleDaPagare = (0.21 * KilometriDaPercorrere).toFixed(2)
+let Prezzo = (0.21 * KilometriDaPercorrere).toFixed(2)
 
-console.log('prezzo',PrezzoNormaleDaPagare + Moneta)
+console.log('prezzo',Prezzo + Moneta)
 
-// Età
+//Età
 
 const Age = prompt ('quanti anni hai')
 console.log('età',Age)
 
-// Calcolo finale
-let PrezzoPerVecchi
+//Calcolo finale
 
-let PrezzoPerGiovini
 
 if (isNaN(Age)) {
     alert('Scrivi un Numero')
-
 }
 if(Age > 65){
-    PrezzoPerVecchi = ((0.21 * KilometriDaPercorrere)*0.6).toFixed(2)
-    console.log('Per Vecchi',PrezzoPerVecchi)
+    Prezzo = ((0.21 * KilometriDaPercorrere)*0.6).toFixed(2)
+    console.log('Per Vecchi',Prezzo)
 }
 if(Age < 18){
-    PrezzoPerGiovini = ((0.21 * KilometriDaPercorrere)*0.8).toFixed(2)
-    console.log('Per Giovini',PrezzoPerGiovini)
+    Prezzo = ((0.21 * KilometriDaPercorrere)*0.8).toFixed(2)
+    console.log('Per Giovini',Prezzo)
 }
+
+//stamp su html
+
+document.getElementById ("prezzo-biglietto").innerHTML = Prezzo + Moneta
